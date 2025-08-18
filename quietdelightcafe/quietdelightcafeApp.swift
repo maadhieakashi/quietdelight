d8708d70786f764c5b807fe4e61fcaed8dcebd58
ma <sahimeshi002@NIBMADMIN04s-iMac.local>
2025-08-18 09:43:17 +0530

    first commit

---
+++ quietdelightcafe/quietdelightcafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  quietdelightcafeApp.swift
+//  quietdelightcafe
+//
+//  Created by SAHimeshi 002 on 2025-08-18.
+//
+
+import SwiftUI
+
+@main
+struct quietdelightcafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ quietdelightcafeTests/quietdelightcafeTests.swift
@@ -0,0 +1,17 @@
+//
+//  quietdelightcafeTests.swift
+//  quietdelightcafeTests
+//
+//  Created by SAHimeshi 002 on 2025-08-18.
+//
+
+import Testing
+@testable import quietdelightcafe
+
+struct quietdelightcafeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
